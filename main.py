83e3ccfcc58507d3d476532782d7d5615993495a
Kevin Jonas Pasaribu <jonaspasaribu3@gmail.com>
2024-02-28 04:13:20 +0700

    tips tipis wae

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import pandas as pd
+import joblib
+
+# trained model load
+model_rf = joblib.load('''NAMA MODEL YANG UDAH DI TRAIN GAES''')
+
+def predict():
+    # Get user inputs from the form
+    features = ['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 
+                'thalach', 'exang', 'oldpeak', 'slope', 'ca', 'thal']
+    
+
 # fokus bikin ppt dlu brokkk
